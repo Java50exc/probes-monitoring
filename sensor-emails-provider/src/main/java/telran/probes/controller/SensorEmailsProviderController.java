d22eaa4b8735280749014deec208d4d115537cbc
allanteone <allanteone@gmail.com>
2024-03-19 08:12:49 +0200

    hw_74 adds controller

---
+++ sensor-emails-provider/src/main/java/telran/probes/controller/SensorEmailsProviderController.java
@@ -0,0 +1,22 @@
+package telran.probes.controller;
+
+import static telran.probes.UrlConstants.*;
+import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.service.SensorEmailsProviderService;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class SensorEmailsProviderController {
+	final SensorEmailsProviderService sensorEmailsProviderService;
+	
+	@GetMapping(EMAILS_PATH + "{sensorId}")
+	String[] getSensorEmails(@PathVariable("sensorId") long sensorId) {
+		String[] emails = sensorEmailsProviderService.getSensorEmails(sensorId);
+		log.debug("SensorEmailsProviderController: received emails {} for id {}", emails, sensorId);
+		return emails;
+	}
+
+}
