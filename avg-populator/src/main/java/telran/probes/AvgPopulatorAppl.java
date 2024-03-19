b7265d71f830ac56dc7ef7bad5ea550cfe483a5e
allanteone <allanteone@gmail.com>
2024-03-19 13:14:27 +0200

    hw_74a avg-populator initializing

---
+++ avg-populator/src/main/java/telran/probes/AvgPopulatorAppl.java
@@ -0,0 +1,36 @@
+package telran.probes;
+
+import java.util.function.Consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.dto.ProbeData;
+import telran.probes.model.ProbeDataDoc;
+import telran.probes.repo.AvgPopulatorRepo;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+public class AvgPopulatorAppl {
+	final AvgPopulatorRepo avgPopulatorRepo;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AvgPopulatorAppl.class, args);
+	}
+	
+	@Bean
+	Consumer<ProbeData> avgPopulatorConsumer() {
+		return probeData -> probeDataPopulation(probeData);
+	}
+
+	private void probeDataPopulation(ProbeData probeData) {
+		log.debug("{}", probeData.id());
+		avgPopulatorRepo.save(new ProbeDataDoc(probeData));
+		
+	}
+
+}
