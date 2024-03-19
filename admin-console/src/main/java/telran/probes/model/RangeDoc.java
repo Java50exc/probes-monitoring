713be3c7c8e6aaf95f030b3522db12bc07a90415
allanteone <allanteone@gmail.com>
2024-03-19 22:58:25 +0200

    hw_74a updates model

--- admin-console/src/main/java/telran/probes/model/RangeDoc.java
+++ admin-console/src/main/java/telran/probes/model/RangeDoc.java
@@ -5,13 +5,25 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import telran.probes.dto.Range;
+import telran.probes.dto.SensorRange;
 
 @Document(collection = "ranges")
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RangeDoc {
 	@Id
 	long id;
 	Range range;
+	
+	public SensorRange build() {
+		return new SensorRange(id, range);	
+	}
+	
+	public RangeDoc(SensorRange sensorRange) {
+		id = sensorRange.id();
+		range = sensorRange.range();
+	}
 }
--- admin-console/src/main/java/telran/probes/model/SensorEmailsDoc.java
+++ admin-console/src/main/java/telran/probes/model/SensorEmailsDoc.java
@@ -5,13 +5,25 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import telran.probes.dto.SensorEmails;
 
 @Document(collection = "emails")
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SensorEmailsDoc {
 	@Id
 	long id;
 	String[] emails;
+	
+	public SensorEmails build() {
+		return new SensorEmails(id, emails);
+	}
+	
+	public SensorEmailsDoc(SensorEmails sensorEmails) {
+		id = sensorEmails.id();
+		emails = sensorEmails.mails();
+	}
 
 }
