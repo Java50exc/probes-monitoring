68485eb701f9d18c7dcf48c5ead9b56a0a8ee385
allanteone <allanteone@gmail.com>
2024-03-19 18:43:49 +0200

    hw_74a admin-console initialization

---
+++ admin-console/src/main/java/telran/probes/AdminConsoleAppl.java
@@ -0,0 +1,12 @@
+package telran.probes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminConsoleAppl {
+	public static void main(String[] args) {
+		SpringApplication.run(AdminConsoleAppl.class, args);
+	}
+
+}
