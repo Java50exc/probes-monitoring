99cfa74c5a1acf4f40ac90f59b00028601452c1f
allanteone <allanteone@gmail.com>
2024-03-20 22:51:18 +0200

    hw_76 adds avg-reducer controller tests

---
+++ avg-reducer/src/test/java/telran/probes/AvgReducerControllerTests.java
@@ -0,0 +1,52 @@
+package telran.probes;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.stream.binder.test.*;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import telran.probes.dto.ProbeData;
+import telran.probes.service.AvgReducerService;
+import static telran.probes.TestsConstants.*;
+
+@SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
+class AvgReducerControllerTests {
+	@Autowired
+	InputDestination producer;
+	@Autowired
+	OutputDestination consumer;
+	@MockBean
+	AvgReducerService reducerService;
+	ObjectMapper mapper = new ObjectMapper();
+	
+
+	@Test
+	void probeDataReducing_nullFromService_none() {
+		when(reducerService.getAvgValue(any(ProbeData.class))).thenReturn(null);
+		
+		producer.send(new GenericMessage<ProbeData>(PROBE_DATA), consumerBindingName);
+		Message<byte[]> message = consumer.receive(100, producerBindingName);
+		assertNull(message);
+	}
+	
+	@Test
+	void probeDataReducing_avgDataFromService_avgDataSent() throws Exception {
+		when(reducerService.getAvgValue(any(ProbeData.class))).thenReturn(VALUE);
+		
+		producer.send(new GenericMessage<ProbeData>(PROBE_DATA), consumerBindingName);
+		Message<byte[]> message = consumer.receive(100, producerBindingName);
+		assertNotNull(message);
+		ProbeData probeAvg = mapper.readValue(message.getPayload(), ProbeData.class);
+		assertEquals(PROBE_DATA.id(), probeAvg.id());
+		assertEquals(PROBE_DATA.value(), probeAvg.value());
+	}
+
+}
---
+++ avg-reducer/src/test/java/telran/probes/TestsConstants.java
@@ -0,0 +1,12 @@
+package telran.probes;
+
+import telran.probes.dto.ProbeData;
+
+public interface TestsConstants {
+	Double VALUE = 100.0;
+	long SENSOR_ID = 123;
+	ProbeData PROBE_DATA = new ProbeData(SENSOR_ID, VALUE, 0);
+	
+	String consumerBindingName = "avgReducerConsumer-in-0";
+	String producerBindingName = "avgReducerProducer-out-0";
+}
