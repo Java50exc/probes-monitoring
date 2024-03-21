761525e9dd9976abe99f4e96f08d84a6d920fabb
allanteone <allanteone@gmail.com>
2024-03-21 16:06:58 +0200

    hw_77 probes initialization

---
+++ probes/src/main/java/telran/probes/ProbesAppl.java
@@ -0,0 +1,38 @@
+package telran.probes;
+
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.RequiredArgsConstructor;
+import telran.probes.dto.ProbeData;
+import telran.probes.service.ProbesService;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class ProbesAppl {
+	private static int TIMEOUT = 10000;
+	
+	final ProbesService probesService;
+	
+	public static void main(String[] args) throws Exception {
+		var ctx = SpringApplication.run(ProbesAppl.class, args);
+		Thread.sleep(TIMEOUT);
+		ctx.close();
+	}
+	
+	
+	@Bean
+	Supplier<ProbeData> probesSupplier() {
+		return () -> probeGeneration();
+	}
+
+
+	private ProbeData probeGeneration() {
+		return probesService.getProbeData();
+	}
+	
+
+}
