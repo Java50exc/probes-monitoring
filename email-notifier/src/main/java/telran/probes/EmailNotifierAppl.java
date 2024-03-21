c0d95745b5be7de542081303818e1308774f88d8
allanteone <allanteone@gmail.com>
2024-03-21 12:19:41 +0200

    cw_77 adds email-notifier controller tests

--- email-notifier/src/main/java/telran/probes/EmailNotifierAppl.java
+++ email-notifier/src/main/java/telran/probes/EmailNotifierAppl.java
@@ -1,18 +1,32 @@
 package telran.probes;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.function.Consumer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.dto.DeviationData;
 import telran.probes.dto.SensorUpdateData;
 import telran.probes.service.EmailsProviderClientService;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@Slf4j
 public class EmailNotifierAppl {
+	@Value("${app.mail.notifier.subject:deviation of sensor }")
+	private String subject;
 	final EmailsProviderClientService clientService;
+	final JavaMailSender mailSender;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EmailNotifierAppl.class, args);
@@ -28,6 +42,44 @@ public class EmailNotifierAppl {
 		if (updateData.emails() != null) {
 			clientService.updateCache(updateData.id(), updateData.emails());
 		}
+	}
+	
+	@Bean
+	Consumer<DeviationData> emailNotifierConsumer() {
+		return deviation -> sendingMail(deviation);
+	}
+
+
+	private void sendingMail(DeviationData deviation) {
+		log.debug("received deviation data: {}", deviation);
+		SimpleMailMessage smm = new SimpleMailMessage();
+		Long sensorId = deviation.id();
+		String[] emails = clientService.getMails(sensorId);
+		log.debug("received mail addresses are: {}", Arrays.toString(emails));
+		
+		smm.setTo(emails);
+		smm.setText(getText(deviation));
+		smm.setSubject(getSubject(sensorId));
+		mailSender.send(smm);
+	}
+
+
+	private String getSubject(Long sensorId) {
+		String result = subject + sensorId;
+		log.debug("subject: {}", subject);
+		return result;
+	}
+
+
+	private String getText(DeviationData deviation) {
+		String text = String.format("sensor %d has value %.2f with deviation %.2f at %s",
+				deviation.id(), deviation.value(), deviation.deviation(), getDateTime(deviation.timestamp()));
+		return text;
+	}
+
+
+	private LocalDateTime getDateTime(long timestamp) {
+		return Instant.ofEpochMilli(timestamp).atZone(ZoneId.systemDefault()).toLocalDateTime();
 	}
 
 }
