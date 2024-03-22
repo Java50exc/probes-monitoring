e79a7ae7a7a33bd098a20e4df3789d21f78abe2b
allanteone <allanteone@gmail.com>
2024-03-22 17:13:45 +0200

    hw_78 fixes admin-console validation bug

--- admin-console/src/main/java/telran/probes/service/AdminConsoleServiceImpl.java
+++ admin-console/src/main/java/telran/probes/service/AdminConsoleServiceImpl.java
@@ -1,5 +1,8 @@
 package telran.probes.service;
 
+import static telran.probes.messages.ErrorMessages.*;
+
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -19,19 +22,21 @@ public class AdminConsoleServiceImpl implements AdminConsoleService {
 
 	@Value("${app.admin.console.producer.binding.name}")
 	String producerBindingName;
-	static final String RANGE_COLLECTION = "ranges";
-	static final String EMAIL_COLLECTION = "emails";
+	@Value("${app.sensor.ranges.collection.name}")
+	String RANGE_COLLECTION;
+	@Value("${app.sensor.emails.collection.name}")
+	String EMAIL_COLLECTION;
 
 	@Override
 	public SensorRange addSensorRange(SensorRange sensorRange) {
-		log.debug("Range {} for sensor with id {} has been added", sensorRange.range(), 1);
+		rangeValidation(sensorRange.range());
 		long id = sensorRange.id();
 
 		try {
 			mongoTemplate.insert(new RangeDoc(sensorRange), RANGE_COLLECTION);
 		} catch (DuplicateKeyException e) {
 			log.error("Range for sensor with id {} already exists", id);
-			throw new SensorRangeIllegalStateException();
+			throw new SensorRangeIllegalStateException(SENSOR_RANGE_ALREADY_EXISTS);
 		}
 		log.debug("Range {} for sensor with id {} has been added", sensorRange.range(), id);
 		return sensorRange;
@@ -39,13 +44,14 @@ public class AdminConsoleServiceImpl implements AdminConsoleService {
 
 	@Override
 	public SensorEmails addSensorEmails(SensorEmails sensorEmails) {
+		emailsValidation(sensorEmails.mails());
 		long id = sensorEmails.id();
 
 		try {
 			mongoTemplate.insert(new SensorEmailsDoc(sensorEmails), EMAIL_COLLECTION);
 		} catch (DuplicateKeyException e) {
 			log.error("Emails for sensor with id {} already exists", id);
-			throw new SensorEmailsIllegalStateException();
+			throw new SensorEmailsIllegalStateException(SENSOR_EMAILS_ALREADY_EXISTS);
 		}
 		log.debug("Emails {} for sensor with id {} has been added", sensorEmails.mails(), id);
 		return sensorEmails;
@@ -53,6 +59,7 @@ public class AdminConsoleServiceImpl implements AdminConsoleService {
 
 	@Override
 	public SensorRange updateSensorRange(SensorRange sensorRange) {
+		rangeValidation(sensorRange.range());
 		long id = sensorRange.id();
 		RangeDoc doc = mongoTemplate.findAndModify(new Query(Criteria.where("id").is(id)),
 				new Update().set("range", sensorRange.range()), RangeDoc.class, RANGE_COLLECTION);
@@ -66,6 +73,7 @@ public class AdminConsoleServiceImpl implements AdminConsoleService {
 
 	@Override
 	public SensorEmails updateSensorEmails(SensorEmails sensorEmails) {
+		emailsValidation(sensorEmails.mails());
 		long id = sensorEmails.id();
 		SensorEmailsDoc doc = mongoTemplate.findAndModify(new Query(Criteria.where("id").is(id)),
 				new Update().set("emails", sensorEmails.mails()), SensorEmailsDoc.class, EMAIL_COLLECTION);
@@ -76,5 +84,21 @@ public class AdminConsoleServiceImpl implements AdminConsoleService {
 		}
 		return sensorEmails;
 	}
+	
+	private void rangeValidation(Range range) {
+		if (range.maxValue() < range.minValue()) {
+			throw new SensorRangeIllegalStateException(RANGE_MIN_GREATER_THEN_MAX);
+		}
+	}
+	
+	private void emailsValidation(String[] emails) { 
+       Arrays.stream(emails).forEach(e -> {
+    	   if (!e.matches(EMAIL_REGEX)) {
+    		   throw new SensorEmailsIllegalStateException(INVALID_EMAIL_FORMAT + e);
+    	   }
+       });
+	}
+
+        
 
 }
