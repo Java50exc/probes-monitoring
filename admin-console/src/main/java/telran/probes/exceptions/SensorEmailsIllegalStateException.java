2f2d4b4bf56acdafd6a2a76557a2d89fdbf3f527
allanteone <allanteone@gmail.com>
2024-03-22 17:14:14 +0200

    hw_78 updates admin-console validation exceptions

--- admin-console/src/main/java/telran/probes/exceptions/SensorEmailsIllegalStateException.java
+++ admin-console/src/main/java/telran/probes/exceptions/SensorEmailsIllegalStateException.java
@@ -1,10 +1,9 @@
 package telran.probes.exceptions;
-import static telran.probes.messages.ErrorMessages.*;
 
 @SuppressWarnings("serial")
 public class SensorEmailsIllegalStateException extends IllegalStateException {
-	public SensorEmailsIllegalStateException() {
-		super(SENSOR_EMAILS_ALREADY_EXISTS);
+	public SensorEmailsIllegalStateException(String message) {
+		super(message);
 	}
 
 }
--- admin-console/src/main/java/telran/probes/exceptions/SensorRangeIllegalStateException.java
+++ admin-console/src/main/java/telran/probes/exceptions/SensorRangeIllegalStateException.java
@@ -1,11 +1,9 @@
 package telran.probes.exceptions;
 
-import static telran.probes.messages.ErrorMessages.*;
-
 @SuppressWarnings("serial")
 public class SensorRangeIllegalStateException extends IllegalStateException {
-	public SensorRangeIllegalStateException() {
-		super(SENSOR_RANGE_ALREADY_EXISTS);
+	public SensorRangeIllegalStateException(String message) {
+		super(message);
 	}
 
 }
