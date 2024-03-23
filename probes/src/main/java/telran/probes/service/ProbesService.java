f72cd84f3ef2eab7e5d4c837ed5019ec4169c9f4
allanteone <allanteone@gmail.com>
2024-03-23 14:00:21 +0200

    hw_79 updates probes service for consuming updates-data and cache sync

--- probes/src/main/java/telran/probes/service/ProbesService.java
+++ probes/src/main/java/telran/probes/service/ProbesService.java
@@ -1,8 +1,9 @@
 package telran.probes.service;
 
-import telran.probes.dto.ProbeData;
+import telran.probes.dto.*;
 
 public interface ProbesService {
 	ProbeData getProbeData();
+	void updateCache(long id, Range range);
 
 }
--- probes/src/main/java/telran/probes/service/ProbesServiceImpl.java
+++ probes/src/main/java/telran/probes/service/ProbesServiceImpl.java
@@ -2,6 +2,7 @@ package telran.probes.service;
 
 import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.springframework.stereotype.Service;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -20,26 +21,32 @@ public class ProbesServiceImpl implements ProbesService {
 	private HashMap<Long, Range> cache = new HashMap<>();
 	private Long[] sensorIds;
 
-
+	private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
 	@Override
 	public ProbeData getProbeData() {
+		ProbeData probeData = null;
+		Range range = null;
+		long sensorId = 0;
 		dbCaching();
-		int index = ThreadLocalRandom.current().nextInt(sensorIds.length);
-		long sensorId = sensorIds[index];
-
-		log.trace("creating probeData with id {}", sensorId);
-
-		ProbeData probeData = getRandomProbeData(cache.get(sensorId), sensorId);
-		log.debug("probeData created: {}", probeData);
-
+		
+		try {
+			lock.readLock().lock();
+			sensorId = sensorIds[ThreadLocalRandom.current().nextInt(sensorIds.length)];
+			range = cache.get(sensorId);
+		} finally {
+			lock.readLock().unlock();
+			log.trace("creating probeData with id {} in range {}", sensorId, range);
+			probeData = getRandomProbeData(range, sensorId);
+			log.debug("probeData created: {}", probeData);
+		}
 		return probeData;
 	}
-	
+
 	private ProbeData getRandomProbeData(Range range, Long sensorId) {
 		ThreadLocalRandom rand = ThreadLocalRandom.current();
 		double value;
-		
+
 		if (rand.nextDouble() <= probesConfig.getDeviationProb()) {
 			value = rand.nextDouble() <= probesConfig.getLessProb() ? range.minValue() - rand.nextDouble(100)
 					: range.maxValue() + rand.nextDouble(100);
@@ -56,35 +63,54 @@ public class ProbesServiceImpl implements ProbesService {
 	private void dbPopulation() {
 		long curCount = probesRepo.count();
 		if (curCount < probesConfig.getNSensors()) {
-			long initialId = probesRepo.findAll().stream().mapToLong(e -> e.getId()).max().orElse(probesConfig.getInitialSensorId());
-			insertDoc(probesConfig.getNSensors() - curCount, initialId + 1, probesConfig.getMinValue(), probesConfig.getMaxValue());
+			long initialId = probesRepo.findAll().stream().mapToLong(e -> e.getId()).max()
+					.orElse(probesConfig.getInitialSensorId());
+			insertDoc(probesConfig.getNSensors() - curCount, initialId + 1, probesConfig.getMinValue(),
+					probesConfig.getMaxValue());
 		}
 	}
-	
+
 	private void dbCaching() {
-		if (cache.size() < probesRepo.count()) {
-			probesRepo.findAll().stream().forEach(e -> cache.put(e.getId(), e.getRange()));
-			sensorIds = cache.keySet().toArray(Long[]::new);
+		try {
+			lock.writeLock().lock();
+			if (cache.size() < probesRepo.count()) {
+				probesRepo.findAll().stream().forEach(e -> cache.put(e.getId(), e.getRange()));
+				sensorIds = cache.keySet().toArray(Long[]::new);
+			}
+		} finally {
+			lock.writeLock().unlock();
 		}
 	}
-	
+
 	private void insertDoc(long docsCount, long initId, double minValue, double maxValue) {
 		ThreadLocalRandom rand = ThreadLocalRandom.current();
 		if (docsCount > 0) {
 			insertDoc(docsCount - 1, initId + 10, minValue, maxValue);
-			SensorRangeDoc doc = new SensorRangeDoc(rand.nextLong(initId, initId + 10), getRandomRange(minValue, maxValue));
+			SensorRangeDoc doc = new SensorRangeDoc(rand.nextLong(initId, initId + 10),
+					getRandomRange(minValue, maxValue));
 			probesRepo.save(doc);
 			log.debug("sensor range {} with id {}, succesfully saved to db", doc.getRange(), doc.getId());
 		}
 	}
-	
+
 	private Range getRandomRange(double minValue, double maxValue) {
 		ThreadLocalRandom rand = ThreadLocalRandom.current();
 		double dif = maxValue - minValue;
 		double step = dif / 2 - dif / 10;
 		minValue -= rand.nextDouble(step) - rand.nextDouble(step);
 		maxValue -= rand.nextDouble(step) - rand.nextDouble(step);
-		return new Range(minValue, maxValue);	
+		return new Range(minValue, maxValue);
+	}
+
+	@Override
+	public void updateCache(long id, Range range) {
+		try {
+			lock.writeLock().lock();
+			cache.put(id, range);
+			sensorIds = cache.keySet().toArray(Long[]::new);
+		} finally {
+			lock.writeLock().unlock();
+		}
 	}
 
 }
