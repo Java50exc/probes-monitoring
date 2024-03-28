962d1f0cac0cf241a08afc32da76a4a0ee1774b0
allanteone <allanteone@gmail.com>
2024-03-28 11:24:27 +0200

    cosmetic fix

--- sensor-range-provider/src/main/java/telran/probes/service/SensorRangeProviderServiceImpl.java
+++ sensor-range-provider/src/main/java/telran/probes/service/SensorRangeProviderServiceImpl.java
@@ -1,7 +1,6 @@
 package telran.probes.service;
 
 import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import telran.probes.dto.Range;
