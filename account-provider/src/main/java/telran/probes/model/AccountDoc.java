26cba19725fc01f0eb0e911c15a93bb21daf31a7
allanteone <allanteone@gmail.com>
2024-03-28 11:23:08 +0200

    hw_82 adds account-provider model

---
+++ account-provider/src/main/java/telran/probes/model/AccountDoc.java
@@ -0,0 +1,20 @@
+package telran.probes.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.*;
+import telran.probes.dto.AccountDto;
+
+@Document(collection = "accounts")
+@AllArgsConstructor
+@Getter
+public class AccountDoc {
+	@Id
+	String username;
+	String hashPassword;
+	String[] roles;
+	
+	public AccountDto toDto() {
+		return new AccountDto(username, hashPassword, roles);
+	}
+}
