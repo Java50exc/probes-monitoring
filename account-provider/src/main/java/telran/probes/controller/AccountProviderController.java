56cb3f0d3eeaa55cb0b4f518a57233674e679e2a
allanteone <allanteone@gmail.com>
2024-03-28 11:23:30 +0200

    hw_82 adds account-provider controller

---
+++ account-provider/src/main/java/telran/probes/controller/AccountProviderController.java
@@ -0,0 +1,25 @@
+package telran.probes.controller;
+
+import static telran.probes.messages.ErrorMessages.*;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.constraints.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.dto.AccountDto;
+import telran.probes.service.AccountProviderService;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class AccountProviderController {
+	final AccountProviderService providerService;
+	
+	@GetMapping("${app.account.provider.path}" + "/{email}")
+	AccountDto getAccount(@PathVariable("email") @Email(message = WRONG_EMAIL_FORMAT) @NotBlank(message = EMPTY_EMAIL) String email) {
+		log.debug("AccountProviderController: getAccount: received {}", email);
+		return providerService.getAccount(email);
+	}
+	
+	
+
+}
