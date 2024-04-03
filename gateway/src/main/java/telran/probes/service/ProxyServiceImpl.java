8f0d33ab9ede5bd36c7d90c8fb86353aa4c16c2b
allanteone <allanteone@gmail.com>
2024-04-03 18:31:02 +0300

    cw_84

---
+++ gateway/src/main/java/telran/probes/service/ProxyServiceImpl.java
@@ -0,0 +1,52 @@
+package telran.probes.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.mvc.ProxyExchange;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class ProxyServiceImpl implements ProxyService {
+
+	@Value("#{${app.map.hosts.ports}}")
+	Map<String, String> routingMap;
+	
+	@Override
+	public ResponseEntity<byte[]> proxyRouting(ProxyExchange<byte[]> proxy, HttpServletRequest request,
+			String httpMethod) {
+		String routedUrl = getRoutedUrl(request);
+		log.debug("routed URL is {}", routedUrl);
+		ResponseEntity<byte[]> res = switch(httpMethod) {
+		case "GET" -> proxy.uri(routedUrl).get();
+		case "POST" -> proxy.uri(routedUrl).post();
+		case "DELETE" -> proxy.uri(routedUrl).delete();
+		case "PUT" -> proxy.uri(routedUrl).put();
+		default -> throw new IllegalArgumentException("Unexpected value: " + httpMethod);
+		};
+		log.debug("returns {}", res);
+		return res;
+	}
+
+	private String getRoutedUrl(HttpServletRequest request) {
+		String resourceName = request.getRequestURI();
+		log.debug("resource name: {}", resourceName);
+		String firstPart = resourceName.split("/")[1];
+		log.debug("first name part is {}", firstPart);
+		String hostPort = routingMap.get(firstPart);
+		
+		return String.format("http://%s%s", hostPort, resourceName);
+	}
+	
+	@PostConstruct
+	void logRoutingMap() {
+		log.debug("routing ,ap is {}", routingMap);
+	}
+
+}
